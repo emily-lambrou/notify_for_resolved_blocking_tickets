b05db0c618c8f5880c7c24e9147ace282682a565
Emily <emily-l93@hotmail.com>
2026-04-06 10:14:51 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -7,16 +7,12 @@ ENTERPRISE_GITHUB = os.getenv("INPUT_ENTERPRISE_GITHUB", "False")
 REPOSITORY_OWNER_TYPE = os.getenv("INPUT_REPOSITORY_OWNER_TYPE", "organization")
 
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY", "")
-OWNER, REPO = GITHUB_REPOSITORY.split("/")
+
+# Split owner and repository name correctly
+OWNER, REPO_NAME = GITHUB_REPOSITORY.split("/")
 
 BASE_URL = "https://github.intranet.unicaf.org/api"
 GRAPHQL_URL = f"{BASE_URL}/graphql"
-
-notification_type = os.environ['INPUT_NOTIFICATION_TYPE']
-
-if notification_type not in ['comment', 'email']:
-    raise Exception(f'Unsupported notification type {notification_type}')
-    
 
 HEADERS = {
     "Authorization": f"Bearer {GH_TOKEN}",
